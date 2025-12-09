a14579b73e1f09aca98f9389a820edb904a2eccb
duyphuong253 <phuongdang.020503@gmail.com>
2025-12-09 19:20:49 +0700

    connect mongodb

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 
+require("dotenv").config();
+
 const route = require("./routes/client/index.route");
 
+const database = require("./config/database.js");
+database.connect();
+
 const app = express();
-const port = 3002;
+const port = process.env.PORT;
+
+app.use(express.static("public"));
 
 // Cấu hình để sử dụng pug
 app.set("views", "./views");
