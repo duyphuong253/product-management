83d4c47c908130368226d205b6d801f48413e557
duyphuong253 <phuongdang.020503@gmail.com>
2025-12-11 18:22:03 +0700

    lấy sản phẩm ở mongoDB in ra giao diện

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,14 @@
-module.exports.index = (req, res) =>{
-     res.render("admin/pages/products/index", {
-        pageTitle: "Danh sách sản phẩm"
+const Product = require("../../models/product.model");
+
+// [GET] /admin/products
+module.exports.index = async (req, res) =>{
+    const products =  await Product.find({
+        deleted: false,
+    });
+    console.log(products);
+
+    res.render("admin/pages/products/index", {
+        pageTitle: "Danh sách sản phẩm",
+        products: products
     });
 }
