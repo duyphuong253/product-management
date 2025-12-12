e11e98515a58bcf43a4152c0c6db736776976f4a
duyphuong253 <phuongdang.020503@gmail.com>
2025-12-12 16:47:29 +0700

    tính năng Phân trang

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 const Product = require("../../models/product.model");
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) =>{
@@ -21,7 +22,19 @@ module.exports.index = async (req, res) =>{
         find.title = objectSearch.regex;
     //  End Search //
 
-    const products =  await Product.find(find);
+    // Pagination //
+    const countProducts = await Product.countDocuments(find);
+    let objectPagination = paginationHelper(
+        {
+        currentPage: 1,
+        limitItem: 4
+        },
+        req.query,
+        countProducts
+    );
+    // End Pagination //
+
+    const products =  await Product.find(find).limit(objectPagination.limitItem).skip(objectPagination.skip);
 
     // console.log(products);
 
@@ -29,6 +42,7 @@ module.exports.index = async (req, res) =>{
         pageTitle: "Danh sách sản phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword: objectSearch.keyword
+        keyword: objectSearch.keyword,
+        pagination: objectPagination
     });
 }
