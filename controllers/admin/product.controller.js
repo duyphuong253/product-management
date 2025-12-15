244cf370a51235b5b08bd3a95415a2aa65c57225
duyphuong253 <phuongdang.020503@gmail.com>
2025-12-15 17:27:38 +0700

    Xóa mềm nhiều sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -68,6 +68,9 @@ module.exports.changeMulti = async (req, res) =>{
         case "inactive":
             await Product.updateMany({_id: { $in: ids }}, {status: "inactive"});
             break;
+        case "delete-all":
+            await Product.updateMany({_id: { $in: ids }}, {deleted: true, deletedAt: new Date()});
+            break;
         default:
             break;
     }
@@ -77,8 +80,12 @@ module.exports.changeMulti = async (req, res) =>{
 // [DELETE] /admin/products/delete/:id
 module.exports.deleteItem = async (req, res) =>{
     const id = req.params.id;
+    // await Product.deleteOne({ _id: id });
 
-    await Product.deleteOne({ _id: id });
+    await Product.updateOne({ _id: id }, {
+        deleted: true,
+        deletedAt: new Date()
+    });
 
     res.redirect("/admin/products");
 };
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -94,6 +94,13 @@ if(formChangeMulti){
         e.preventDefault();
         const checkboxMulti = document.querySelector("[checkbox-multi]");
         const inputsChecked = checkboxMulti.querySelectorAll("input[name='id']:checked");
+
+        const typeChange = e.target.elements.type.value;
+        if(typeChange == "delete-all"){
+            const isConfirm = confirm("Bạn có chắc muốn xóa tất cả sản phẩm này?");
+            if(!isConfirm)
+                return;
+        }
         
         if(inputsChecked.length > 0){
             let ids = [];
