20545da709e816132ffed862903f8d2b91c0d55e
duyphuong253 <phuongdang.020503@gmail.com>
2025-12-16 18:44:06 +0700

    Trang chi tiết bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -2,20 +2,39 @@ const Product = require("../../models/product.model");
 
 // [GET] /products
 module.exports.index = async (req, res) =>{
-    const products = await Product.find
-    .sort({position: "desc"})({
+    const products = await Product.find({
         status: "active",
         deleted: false
-    });
-    const newPrice = products.map(item => {
-        item.priceNew = (item.price * (100 - item.discountPercentage) / 100).toFixed(0);
-        return item;
-    })
+    }).sort({ position: "desc" });
 
-    console.log(newPrice);
-    
+    const newPrice = products.map(item => {
+        item.priceNew = Math.round(
+            item.price * (100 - item.discountPercentage) / 100
+        );
+        return item;
+    });
+
     res.render("client/pages/products/index", {
         pageTitle: "Danh sách sản phẩm",
         products: newPrice
     });
-}
+};
+
+// [GET] /products/:slug
+module.exports.detail = async (req, res) =>{
+    try{
+        const find = {
+            deleted: false,
+            slug: req.params.slug
+        }
+    
+        const product = await Product.findOne(find);
+        console.log(product);
+        res.render("client/pages/products/detail", {
+            pageTitle: product.title,
+            product: product
+        });
+        }catch(error){
+            res.redirect(`/products`);
+        }
+};
