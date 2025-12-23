8fe94a3f8dc1a1d0da951d914fadaae73f1af3b7
duyphuong253 <phuongdang.020503@gmail.com>
2025-12-23 11:32:23 +0700

    Phân quyền bên sider

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,16 +1,21 @@
 const systemConfig = require("../../config/system");
 const Account = require("../../models/account.model");
+const Role = require("../../models/roles.model");
 module.exports.requireAuth = async (req, res, next) =>{
     // nếu token không có trong trang dashboard thì sẽ tự logout
     if(!req.cookies.token){
         res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
     } else{
-        console.log(req.cookies.token);
-        const user = await Account.findOne({ token: req.cookies.token});
+        const user = await Account.findOne({ token: req.cookies.token}).select("-password");
         if(!user){
             res.clearCookie("token");
             res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
         } else{
+            const role = await Role.findOne({
+                _id: user.role_id
+            }).select("title permissions");
+            res.locals.user = user;
+            res.locals.role = role;
             next();
         }
     }
