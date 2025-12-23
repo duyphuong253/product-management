89bb44e4461e5673305ce12f7e7035932de6e7fe
duyphuong253 <phuongdang.020503@gmail.com>
2025-12-23 16:58:41 +0700

    trang thông tin cá nhân bên admin

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -5,6 +5,7 @@ const productCategoryRoutes = require("./products-category.route");
 const roleRoutes = require("./role.route");
 const accountRoutes = require("./account.route");
 const authRoutes = require("./auth.route");
+const myAccountRoutes = require("./my-account.route");
 const authMiddleware = require("../../middlewares/admin/auth.middleware");
 
 
@@ -26,6 +27,8 @@ module.exports = (app) =>{
     app.use(PATH_ADMIN + "/accounts", authMiddleware.requireAuth, accountRoutes);
 
     app.use(PATH_ADMIN + "/auth", authRoutes);
+
+    app.use(PATH_ADMIN + "/my-account", authMiddleware.requireAuth, myAccountRoutes);
 
 }
 
