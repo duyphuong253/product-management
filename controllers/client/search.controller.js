0f8dc5b3ca10a39ce2b96047395a1500e9d10936
duyphuong253 <phuongdang.020503@gmail.com>
2025-12-25 19:19:32 +0700

    Tính năng tìm kiếm sản phẩm (client)

---
+++ controllers/client/search.controller.js
@@ -0,0 +1,24 @@
+const Product = require("../../models/product.model");
+const ProductHelper = require("../../helpers/product");
+// [GET] /search
+module.exports.index = async (req, res) =>{
+    const keyword = req.query.keyword;
+
+    let newProducts = [];
+
+    if(keyword){
+        const regex = new RegExp(keyword, "i");
+        const products = await Product.find({
+            title: regex,
+            deleted: false,
+            status: "active",
+        });
+        newProducts = ProductHelper.priceNewProducts(products);
+    }
+
+    res.render("client/pages/search/index", {
+        pageTitle: "Kết quả tìm kiếm",
+        keyword: keyword,
+        products: newProducts,
+    });
+};
