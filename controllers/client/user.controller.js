f6850a76f8f675cd76ec450c3eb4bcc8cac3f408
duyphuong253 <phuongdang.020503@gmail.com>
2025-12-26 22:00:03 +0700

    Đăng ký user

---
+++ controllers/client/user.controller.js
@@ -0,0 +1,30 @@
+const User = require("../../models/user.model");
+const md5 = require("md5");
+// [GET] /user/register
+module.exports.register = async (req, res) =>{
+    res.render("client/pages/user/register", {
+        pageTitle: "Đăng ký tài khoản",
+    })
+}
+
+// [POST] /user/register
+module.exports.registerPost = async (req, res) =>{
+    const exitsEmail = await User.findOne({
+        email: req.body.email,
+    })
+
+    if(exitsEmail){
+        req.flash("error", "Email đã tồn tại!");
+        res.redirect(req.get("referer") || "/");
+        return;
+    }
+
+    req.body.password = md5(req.body.password);
+    const user = new User(req.body);
+    await user.save(user);
+
+    res.cookie("tokenUser", user.tokenUser);
+
+    res.redirect("/");
+    
+}
