c1a06202bfedc31c6a4b06f40a14c04d27b6b6e4
duyphuong253 <phuongdang.020503@gmail.com>
2025-12-26 23:04:56 +0700

    tính năng đăng nhập

--- validates/client/user.validate.js
+++ validates/client/user.validate.js
@@ -1,18 +1,33 @@
 module.exports.registerPost = (req, res, next) =>{
     if(!req.body.fullName){
         req.flash("error", `Vui lòng nhập họ tên!`);
-        res.redirect("/admin/accounts/create");
+        res.redirect(req.get("referer") || "/");
         return;
     }
     if(!req.body.email){
         req.flash("error", `Vui lòng nhập email!`);
-        res.redirect("/admin/accounts/create");
+        res.redirect(req.get("referer") || "/");
         return;
     }
     if(!req.body.password){
         req.flash("error", `Vui lòng nhập emật khẩu!`);
-        res.redirect("/admin/accounts/create");
+        res.redirect(req.get("referer") || "/");
         return;
     }
     next();
 };
+
+module.exports.loginPost = (req, res, next) =>{
+    if(!req.body.email){
+        req.flash("error", `Vui lòng nhập email!`);
+        res.redirect(req.get("referer") || "/");
+        return;
+    }
+    if(!req.body.password){
+        req.flash("error", `Vui lòng nhập emật khẩu!`);
+        res.redirect(req.get("referer") || "/");
+        return;
+    }
+    next();
+};
+
