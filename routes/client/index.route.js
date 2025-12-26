0ee2bb1a5c1204a8b62e40546c72e3152c9f9b85
duyphuong253 <phuongdang.020503@gmail.com>
2025-12-26 17:13:07 +0700

    tính năng đặt hàng

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -4,6 +4,7 @@ const categoryMiddleware = require("../../middlewares/client/category.middleware
 const cartMiddleware = require("../../middlewares/client/cart.middleware");
 const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
+const checkoutRoutes = require("./checkout.route");
 module.exports = (app) =>{
     app.use(categoryMiddleware.category);
 
@@ -16,5 +17,7 @@ module.exports = (app) =>{
     app.use("/search", searchRoutes);
 
     app.use("/cart", cartRoutes);
+
+    app.use("/checkout", checkoutRoutes);
 }
 
